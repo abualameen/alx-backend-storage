2c438134910d9b7f74c367c9ee9097709cf565f8
abu al ameen <abulyaqs@gmail.com>
2024-03-21 15:11:17 +0100

    new error

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,4 @@
+-- Initial setup: Import table dump and create table structure
+
 -- Create index idx_name_first_score on the first letter of names and score
 CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
