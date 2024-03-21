c9f866d7cef07616f6da598f555dc7f99673e07d
abu al ameen <abulyaqs@gmail.com>
2024-03-21 15:25:38 +0100

    new error

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,6 +1,6 @@
 -- Import the SQL dump file to create the 'names' table
 mysql -u root -p holberton < names.sql
 
--- Create the index idx_name_first_score on the table 'names' for the first letter of name and score
+-- Create the index idx_name_first_score on the table 'names' for the first letter of name and the score
 USE holberton;
 CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
