d1e927c59e5d11f4278f376967d810f6260835f2
abu al ameen <abulyaqs@gmail.com>
2024-03-22 18:53:08 +0100

    new corection

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -4,3 +4,6 @@ mysql -u root -p holberton < names.sql
 -- Create the index idx_name_first_score on the table 'names' for the first letter of name and the score
 USE holberton;
 CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), LEFT(score, 1));
+
+-- Show the index details for verification
+SHOW INDEX FROM names;
