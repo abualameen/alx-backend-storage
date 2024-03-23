3cc6460a1796d855521244108db594566c5b6ba5
abu al ameen <abulyaqs@gmail.com>
2024-03-23 11:31:21 +0100

    new corection

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,28 @@
--- Create stored procedure ComputeAverageScoreForUser
-DELIMITER //
+-- Create Procedure to Compute Average Score for User
+DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
+    DECLARE total_score FLOAT;
+    DECLARE score_count INT;
     DECLARE avg_score FLOAT;
-    
+
+    -- Calculate total score for the user
+    SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
+
+    -- Calculate count of scores for the user
+    SELECT COUNT(score) INTO score_count FROM corrections WHERE user_id = user_id;
+
     -- Calculate average score
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-    
-    -- Update user's average score
+    IF score_count > 0 THEN
+        SET avg_score = total_score / score_count;
+    ELSE
+        SET avg_score = 0;
+    END IF;
+
+    -- Update average score in users table
     UPDATE users SET average_score = avg_score WHERE id = user_id;
-END //
+    
+END$$
 
 DELIMITER ;
