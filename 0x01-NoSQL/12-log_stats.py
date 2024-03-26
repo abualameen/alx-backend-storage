71c5c33f6ca1cd13785cd07ddc3528175e7108e3
abu al ameen <abulyaqs@gmail.com>
2024-03-26 23:11:22 +0100

    task 12 adjustment

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,6 +2,7 @@
 """
 Task 12's module.
 """
+
 from pymongo import MongoClient
 
 
@@ -9,24 +10,30 @@ def print_nginx_request_logs(nginx_collection):
     """
     Prints stats about Nginx request logs.
     """
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
+    try:
+        total_logs = nginx_collection.count_documents({})
+        print('{} logs'.format(total_logs))
+        print('Methods:')
+        methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+        for method in methods:
+            req_count = nginx_collection.count_documents({'method': method})
+            print('\tmethod {}: {}'.format(method, req_count))
+        status_checks_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
+        print('{} status check'.format(status_checks_count))
+    except Exception as e:
+        print("Error:", e)
 
 
 def run():
     """
     Provides some stats about Nginx logs stored in MongoDB.
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        nginx_collection = client.logs.nginx
+        print_nginx_request_logs(nginx_collection)
+    except Exception as e:
+        print("Error:", e)
 
 
 if __name__ == '__main__':
